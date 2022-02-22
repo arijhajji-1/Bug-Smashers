a6f06791f24042597d192440fe93b5f3358ce1e3
Hsine Mtiraoui <61585135+hsinemt@users.noreply.github.com>
2022-02-22 21:11:52 +0100

    entity publication

---
+++ src/Entity/Publication.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PublicationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PublicationRepository::class)
+ */
+class Publication
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $desription;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="publications")
+     */
+    private $users;
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDesription(): ?string
+    {
+        return $this->desription;
+    }
+
+    public function setDesription(string $desription): self
+    {
+        $this->desription = $desription;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsers(): ?User
+    {
+        return $this->users;
+    }
+
+    /**
+     * @param mixed $users
+     */
+    public function setUsers(?User $users): self
+    {
+        $this->users = $users;
+        return $this;
+    }
+
+}
